910db4501bfc5f1d73fe2294240487f5c3ffb407
Maxwell-Axfi <lelia.vaultier@gmail.com>
2023-06-23 16:16:00 +0200

    Ajout des Event Listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,15 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//Flèche gauche
+const flecheGauche = document.querySelector(".arrow_left");
+flecheGauche .addEventListener("click", function () {
+	return console.log("Clic sur flèche gauche");
+});
+
+//Flèche droite
+const flecheDroite = document.querySelector(".arrow_right");
+flecheDroite .addEventListener("click", function () {
+	return console.log("Clic sur flèche droite");
+});
